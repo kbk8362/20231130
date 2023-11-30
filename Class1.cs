736c51fc482493a1e70f2da24e4a2143658af0e5
kbk8362 <kbk8362@naver.com>
2023-11-30 10:26:45 +0900

    초기 커밋.

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿
+void quick_sort(int list[], int left, int right)
+{
+    if (left < right)
+    {
+        int q = partition(list, left, right);
+        quick_sort(list, left, q - 1);
+        quick_sort(list, q + 1, right);
+    }
+}
